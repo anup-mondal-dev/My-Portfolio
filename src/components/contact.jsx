f6abfdac2bc785f428947853346ca0d85e44b295
Anup Mondal <mondalanup868@gmail.com>
2025-12-01 21:43:00 +0530

    new changes

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -5,6 +5,7 @@ function contact() {
     <div>
       <div>Contact</div>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex ducimus non harum repellat quae, dolores aliquam facilis! Dignissimos ducimus aspernatur modi animi voluptatem porro, velit neque. Fugiat incidunt temporibus commodi? Harum perferendis provident vel quod reprehenderit ipsum saepe quo temporibus, dolore nisi autem quam rem similique suscipit facilis reiciendis. Dolore.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, molestiae.</p>
     </div>
   )
 }
